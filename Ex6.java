8c79dfe4b5261763044da006683979ee5dee0571
luanagreys7 <luanagreys7@gmail.com>
2025-08-17 21:37:39 -0300

    Ex6

--- Ex6.java
+++ Ex6.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Ex6 extends JFrame {
 
@@ -10,9 +11,28 @@ public class Ex6 extends JFrame {
         setLocationRelativeTo(null);
 
 
-        // Cria o painel principal
+        // Painel esquerdo (vermelho)
+        JPanel painelEsquerdo = new JPanel();
+        painelEsquerdo.setBackground(Color.RED);
+        painelEsquerdo.add(new JLabel("Esquerda", SwingConstants.CENTER));
 
+        // Painel direito (azul)
+        JPanel painelDireito = new JPanel();
+        painelDireito.setBackground(Color.BLUE);
+        painelDireito.add(new JLabel("Direita", SwingConstants.CENTER));
 
+        JSplitPane splitPane = new JSplitPane(
+            JSplitPane.HORIZONTAL_SPLIT, // Divisão vertical (esqerda/direita)
+            painelEsquerdo, 
+            painelDireito
+        );
+
+        // Divisória é posicionada a 40% da largura da janela
+        splitPane.setDividerLocation(0.4); 
+
+        // Adiciona o JSplitPane ao frame
+        add(splitPane);
+        
         // Torna o frame visível
         setVisible(true);
     }
